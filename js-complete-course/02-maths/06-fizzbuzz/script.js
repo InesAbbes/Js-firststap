20f24c0074dbc6b0141a12d852f5cc83266200b2
ines <inesabbes173@gmail.com>
2019-08-21 16:05:26 +0200

    Finish 03-colors

--- js-complete-course/02-maths/06-fizzbuzz/script.js
+++ js-complete-course/02-maths/06-fizzbuzz/script.js
@@ -11,6 +11,18 @@
 
 (function() {
 
-    // your code here
+    for(let i = 1; i<101; i++){
+        // console.log(i);
+        if (i % 3 === 0 && i % 5 === 0) {   // multiple of 3 & 5
+            console.log("fizzbuzz")
+        } else if (i % 3 === 0) {// multiple of 3 & 5
+            console.log("buzz")
+        } else if (i % 5 === 0) {
+            console.log("fizz")
+        } else {
+            console.log(i)
+        }
+    }
 
 })();
+
