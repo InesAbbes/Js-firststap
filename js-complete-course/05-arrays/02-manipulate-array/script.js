a7a5bc83e4313018ede348ae9e320c4c79b86305
ines <inesabbes173@gmail.com>
2019-08-23 12:25:38 +0200

    finished 5.2

--- js-complete-course/05-arrays/02-manipulate-array/script.js
+++ js-complete-course/05-arrays/02-manipulate-array/script.js
@@ -24,6 +24,15 @@
         "cerise",
     ];
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function() {
+        fruits.shift();
+        fruits.pop();
+        fruits.unshift("banana");
+        fruits.push("kiwi");
+
+        alert(fruits);
+
+    });
+
 
 })();
