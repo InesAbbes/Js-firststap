faa6763d4cd82bee0362e1b5632fbf83dc5b236f
ines <inesabbes173@gmail.com>
2019-08-28 16:22:42 +0200

    finishe 6.8

--- js-complete-course/06-dom/08-generate-table-two/script.js
+++ js-complete-course/06-dom/08-generate-table-two/script.js
@@ -11,6 +11,24 @@
 
 (function() {
 
-    // your code here
+
+    // create an HTML table with 1 column and 10 rows.
+    let target = document.getElementById("target");
+
+    let table = document.createElement("table");
+    table.setAttribute("id", "myTable");
+    table.setAttribute("border",1);
+
+    target.appendChild(table);
+    for(let r=0; r<10; r++){
+
+        let row =document.getElementById("myTable").insertRow(r);
+
+        for(let c=0; c<10; c++){
+            let col = row.insertCell(c);
+
+            col.innerHTML = (r+1) * (c+1) ;
+        }
+    }
 
 })();
