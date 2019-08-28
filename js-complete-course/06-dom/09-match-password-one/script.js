d2a1b73a67238660bf58146efcde9e75bf6220c6
ines <inesabbes173@gmail.com>
2019-08-28 16:24:36 +0200

    finishe 6.9

--- js-complete-course/06-dom/09-match-password-one/script.js
+++ js-complete-course/06-dom/09-match-password-one/script.js
@@ -9,8 +9,19 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function () {
+
+
+        let passOne = document.getElementById("pass-one");
+        let passTwo = document.getElementById("pass-two");
+
+        if (passOne.value !== passTwo.value) {
+            passOne.style.borderColor = "red";
+            passTwo.style.borderColor = "red";
+        }
+
+    })
 
 })();
