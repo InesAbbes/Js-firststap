d6a0572e4a12524e9dc37d219216c6f213769181
ines <inesabbes173@gmail.com>
2019-08-28 15:56:55 +0200

    finishe 6.7

--- js-complete-course/06-dom/07-generate-table-one/script.js
+++ js-complete-course/06-dom/07-generate-table-one/script.js
@@ -8,9 +8,22 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
-
 (function() {
 
-    // your code here
+    // create an HTML table with 1 column and 10 rows.
+    let target = document.getElementById("target");
 
+    let table = document.createElement("table");
+
+    table.setAttribute("id", "myTable");
+
+    table.setAttribute("border",1);
+    target.appendChild(table);
+
+    for(let r=0; r<10; r++){
+        let row =document.getElementById("myTable").insertRow(r);
+        for(let c=0; c<10; c++){
+            let col = row.insertCell(c);
+        }
+    }
 })();
