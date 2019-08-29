5be859b65868e26ec38bf0fa4ddb9999a6471d83
ines <inesabbes173@gmail.com>
2019-08-29 11:33:16 +0200

    finishe 6.12

--- js-complete-course/06-dom/12-change-event-input-two/script.js
+++ js-complete-course/06-dom/12-change-event-input-two/script.js
@@ -9,8 +9,21 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // your code here
+    (function() {
 
-})();
+        let inputElement = document.getElementById("pass-one");
+        let counter = document.getElementById("validity");
+        let number = new RegExp( /[0-9].*[0-9]/ );
+
+        inputElement.setAttribute("maxlength", "10");
+
+        inputElement.addEventListener("input", function() {
+            if (inputElement.value.length >= 8 && inputElement.value.match(number)) {
+                counter.innerHTML = "ok";
+            } else {
+                counter.innerHTML = "Not ok";
+            }
+        });
+
+    })();
