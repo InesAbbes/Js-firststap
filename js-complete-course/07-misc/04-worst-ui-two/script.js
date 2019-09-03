b8ef7fc3cec0f43b8883adc15a9ab9c772aa2fd1
ines <inesabbes173@gmail.com>
2019-09-03 16:35:47 +0200

    update chapter 7 (ex 4, 5 , 6).

--- js-complete-course/07-misc/04-worst-ui-two/script.js
+++ js-complete-course/07-misc/04-worst-ui-two/script.js
@@ -9,8 +9,105 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
 (function() {
 
+    var target = document.getElementById("target");
+    var one = document.getElementById("part-one");
+    var two = document.getElementById("part-two");
+    var three = document.getElementById("part-three");
+    var four = document.getElementById("part-four");
 
+    var first = one.innerHTML;
+    var second = two.innerHTML;
+    var third = three.innerHTML;
+    var fourth = four.innerHTML;
 
+    one.innerHTML = "4" + 60;
+
+    first = "60";
+    second = "00";
+    third = "00";
+    fourth = "00";
+
+    target.innerHTML = "04" + first + second + third + fourth;
+
+    one.addEventListener("click", function(){
+
+        if(first === "4" + 99 ){
+            one.innerHTML = 59;
+        }
+
+        first++;
+
+        first = "4" + parseInt(one.innerHTML);
+
+        one.innerHTML++;
+        first = "4" + one.innerHTML.slice(-2);
+        one.innerHTML = first;
+        target.innerHTML = "0" + first + second + third + fourth;
+    });
+
+    two.addEventListener("click", function(){
+
+        first = "4" + parseInt(one.innerHTML);
+        first = "4" + one.innerHTML.slice(-2);
+        one.innerHTML = first;
+
+        second++;
+
+        second = parseInt(two.innerHTML);
+
+        two.innerHTML++;
+        if(second < 9 ){
+            second = "0" + two.innerHTML.slice(-2);
+        }
+        else{
+            second = two.innerHTML.slice(-2);
+        }
+        two.innerHTML = second;
+        target.innerHTML = "0" + first + second + third + fourth;
+    });
+
+    three.addEventListener("click", function(){
+
+        first = "4" + parseInt(one.innerHTML);
+        first = "4" + one.innerHTML.slice(-2);
+        one.innerHTML = first;
+
+        third++;
+
+        third = parseInt(three.innerHTML);
+
+        three.innerHTML++;
+        if(third < 9 ){
+            third = "0" + three.innerHTML.slice(-2);
+        }
+        else{
+            third = three.innerHTML.slice(-2);
+        }
+        three.innerHTML = third;
+        target.innerHTML = "0" + first + second + third + fourth;
+    });
+
+    four.addEventListener("click", function(){
+
+        first = "4" + parseInt(one.innerHTML);
+        first = "4" + one.innerHTML.slice(-2);
+        one.innerHTML = first;
+
+        fourth++;
+
+        fourth = parseInt(four.innerHTML);
+
+        four.innerHTML++;
+        if(fourth < 9 ){
+            fourth = "0" + four.innerHTML.slice(-2);
+        }
+        else{
+            fourth = four.innerHTML.slice(-2);
+        }
+        four.innerHTML = fourth;
+        target.innerHTML = "0" + first + second + third + fourth;
+    });
 })();
--- js-complete-course/07-misc/05-worst-ui-three/script.js
+++ js-complete-course/07-misc/05-worst-ui-three/script.js
@@ -10,9 +10,69 @@
 // You will have time to focus on it later.
 
 (function() {
-    Array.from(document.getElementById("buttom")).forEach((function ()
-    {
-        var inputField = element.parentElement.getElementsByTagName()
-    }
 
-    }));
+    var target = document.getElementById("target");
+
+    var one = document.getElementById("part-one");
+    var two = document.getElementById("part-two");
+    var three = document.getElementById("part-three");
+    var four = document.getElementById("part-four");
+
+    var firstbutton = document.getElementById("fix-part-one");
+    var secondbutton = document.getElementById("fix-part-two");
+    var thirdbutton = document.getElementById("fix-part-three");
+    var fourthbutton = document.getElementById("fix-part-four");
+
+    var firstmin = one.dataset.min;
+    var firstmax = one.dataset.max;
+    var min = two.dataset.min;
+    var max = two.dataset.max;
+
+    firstmin = Math.ceil(firstmin);
+    firstmax = Math.floor(firstmax);
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    var first = "460";
+    var second = "00";
+    var third = "00";
+    var fourth = "00";
+
+    target.innerHTML = "0" + first + second + third + fourth;
+
+
+    firstbutton.addEventListener("click", function(){
+
+        first = Math.floor(Math.random() * (firstmax - firstmin + 1)) + firstmin;
+        one.value = first;
+
+        target.innerHTML = "0" + first + second + third + fourth;
+    });
+
+    secondbutton.addEventListener("click", function(){
+
+        second = ("" + ("100" + Math.floor(Math.random() * (max - min + 1)) + min).slice(-3)).slice(0, 2);
+        two.value = second;
+
+        target.innerHTML = "0" + first + second + third + fourth;
+    });
+
+    thirdbutton.addEventListener("click", function(){
+
+        third = ("" + ("100" + Math.floor(Math.random() * (max - min + 1)) + min).slice(-3)).slice(0, 2);
+        three.value = third;
+
+        target.innerHTML = "0" + first + second + third + fourth;
+    });
+
+    fourthbutton.addEventListener("click", function(){
+
+        fourth = ("" + ("100" + Math.floor(Math.random() * (max - min + 1)) + min).slice(-3)).slice(0, 2);
+        four.value = fourth;
+
+        target.innerHTML = "0" + first + second + third + fourth;
+    });
+
+
+
+})();
