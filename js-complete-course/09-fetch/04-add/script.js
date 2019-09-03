88d5d44603e71ba00a96a0987a8ca259b4edde76
ines <inesabbes173@gmail.com>
2019-09-03 14:25:00 +0200

    Finished 9.4

--- js-complete-course/09-fetch/04-add/script.js
+++ js-complete-course/09-fetch/04-add/script.js
@@ -10,5 +10,23 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    fetch ("http://localhost:63342/Js-firststap/js-complete-course/_shared/api.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (list) {
+    document.getElementById("run").addEventListener("click",function () {
+        let heroName = document.getElementById("hero-name").value;
+        let ego = document.getElementById("hero-alter-ego").value;
+        let powers =document.getElementById("hero-powers").value;
+
+         let newheroId = list.heroes.length +1;
+         list.heroes.push({"id": newheroId ,
+                           "name": heroName,
+                           "alterEgo":  ego,
+                           "abilities": powers,});
+
+                 console.log(list.heroes);
+    });
+     });
 })();
