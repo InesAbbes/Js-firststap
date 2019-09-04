71ced5b2ad71c9381d8852ea5099a0abb091d049
ines <inesabbes173@gmail.com>
2019-09-04 11:25:58 +0200

    finished 8.2

--- js-complete-course/08-asynchronous/02-get-comments/script.js
+++ js-complete-course/08-asynchronous/02-get-comments/script.js
@@ -8,7 +8,31 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
-
 (() => {
-    // your code here
+let posts, comment;
+
+document.getElementById("run").addEventListener("click", function () {
+
+    window.lib.getPosts(function (error, articles) {
+
+        posts = articles;
+
+        posts.forEach(function (post) {
+
+            window.lib.getComments(null, function (error, getComments) {
+
+                comment = getComments[post.id];
+
+                if (comment !== undefined) {
+
+                    post.comment = comment;
+
+                }
+
+                console.log(post);
+
+            });
+        });
+    });
+});
 })();
